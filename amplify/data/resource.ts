ee26862b3219f91aa96442a23809301e6c0a827c
Shun Han <shunhan@umich.edu>
2024-09-22 20:37:11 -0400

    the notes app

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,10 +6,13 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
+
+/*
 const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+      isDone: a.boolean()
     })
     .authorization((allow) => [allow.guest()]),
 });
@@ -20,6 +23,25 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: 'iam',
+  },
+});
+*/
+const schema = a.schema({
+  Note: a
+      .model({
+        name:a.string(),
+        description: a.string(),
+        image: a.string(),
+      })
+      .authorization((allow) => [allow.owner()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
